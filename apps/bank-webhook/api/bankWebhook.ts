6a1a5629ec25ec75a0c32fbd1caf0047c0597a09
S31G <gangwarshrey.31@gmail.com>
2025-03-15 19:30:02 +0530

    please-work

--- apps/bank-webhook/api/bankWebhook.ts
+++ apps/bank-webhook/api/bankWebhook.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import { Request, Response } from "express";
 import prisma from "@repo/db/client";
 import { z } from "zod";
 
@@ -8,14 +8,13 @@ const PaymentSchema = z.object({
   amount: z.string()
 });
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // Enable CORS
+export default async function handler(req: Request, res: Response) {
+
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-  // Handle preflight OPTIONS request
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
